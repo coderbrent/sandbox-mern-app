9300d172b492d5fb1f4fa0e868c59d196fc49aca
Brent Abruzese <brent.abruzese@gmail.com>
2020-02-28 16:45:00 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 // const userRoutes = require('../server/routes/userRoutes')
+const publicPath = path.join(__dirname, '..', 'public');
 const locationRoutes = require('../server/routes/locationRoutes')
 const tripRoutes = require('../server/routes/tripRoutes')
 const driverRoutes = require('../server/routes/driverRoutes')
@@ -13,6 +14,8 @@ const vehicleRoutes = require('../server/routes/vehicleRoutes')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
+
+app.use(express.static(publicPath));
 
 // app.use('/users', userRoutes);
 app.use('/locations', locationRoutes)
@@ -30,6 +33,10 @@ mongoose.connect
     })
   .then(() => console.log('Mongo is connected and working...'))
   .catch(err => console.log(err));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => { console.log('DB is connected') })
